cfcb7038dd59a3ead443f7366cd8469cbe9bc642
jan <jh@datenfreunde.com>
2017-06-10 19:23:20 +0200

    got centroids

--- cluster_items_k_means.py
+++ cluster_items_k_means.py
@@ -9,7 +9,7 @@ imported_collection = imported_db['imported']
 
 imported_news = 'imported'
 
-all_tags = {}
+all_tags_with_items = {}
 
 items = {}
 
@@ -36,21 +36,41 @@ for fn in os.listdir(imported_news):
                 if not item_tags.get(entity_uri, False):
                     item_tags[entity_uri] = entity
 
-                    if not all_tags.get(entity_uri, False):
-                        all_tags[entity_uri] = {
+                    if not all_tags_with_items.get(entity_uri, False):
+                        all_tags_with_items[entity_uri] = {
                             'label': entity['label'],
                             'items': [d['item']['fullText']]}
                     else:
-                        all_tags[entity_uri]['items'].append(d['item']['fullText'])
+                        all_tags_with_items[entity_uri]['items'].append(d['item']['fullText'])
 
                 # append to item overview
                 if not items.get(d['_id'], False):
                     items[d['_id']] = {'tags': item_tags,
                                        'item': d}
 
+from numpy import array
+from scipy.cluster.vq import vq, kmeans, whiten
+
+all_bin_tags = []
+
+for item in items.values():
+    #print(item)
+    binary_tags = [1 if x in item['tags'] else 0 for x in all_tags_with_items.keys()]
+    #print(binary_tags)
+    print('items bin tags: %s' % len(binary_tags))
+    all_bin_tags.append(binary_tags)
+
+print('len of all items: %s ' % len(all_bin_tags))
+
+features = array(all_bin_tags)
+whitened = whiten(features)
+
+# book = array((whitened[0],whitened[2]))  # should be the k guess (fixed to 20 because was 2)
+
+print(len(kmeans(whitened, 20)[0]))
+
+for centroid in (kmeans(whitened, 20)[0]):
+    print(centroid)
+
 print('_____________')
-for tag_id, value in all_tags.items():
-    length = len(value['items'])
-    if length != 1:
-        print(value['label'])
-        print(length)
+#print(all_tags.items())
